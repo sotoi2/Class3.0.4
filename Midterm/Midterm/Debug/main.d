3ba042ae019eca697f3e2e49949940a8b00778e9
sotoi2 <47646947+sotoi2@users.noreply.github.com>
2019-10-27 00:08:40 -0700

    Add files via upload

---
+++ Midterm/Midterm/Debug/main.d
@@ -0,0 +1,69 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdbool.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdlib.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/linkage.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdio.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdarg.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdarg.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/inc/hw_gpio.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/gpio.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/interrupt.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/uart.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/i2c.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/sensorlib/i2cm_drv.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/pin_map.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/inc/hw_memmap.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/sensorlib/hw_mpu6050.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/sensorlib/mpu6050.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/inc/hw_ints.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/inc/hw_sysctl.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/inc/hw_types.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/inc/hw_i2c.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/rom_map.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/debug.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/sysctl.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/string.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/utils/uartstdio.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdarg.h
+main.obj: C:/ti/tivaware_c_series_2_1_4_178/IQmath/IQmathLib.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/limits.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdlib.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/math.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/_defs.h
+
+../main.c: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdbool.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdlib.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/linkage.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdio.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdarg.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdarg.h: 
+C:/ti/tivaware_c_series_2_1_4_178/inc/hw_gpio.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/gpio.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/interrupt.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/uart.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/i2c.h: 
+C:/ti/tivaware_c_series_2_1_4_178/sensorlib/i2cm_drv.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/pin_map.h: 
+C:/ti/tivaware_c_series_2_1_4_178/inc/hw_memmap.h: 
+C:/ti/tivaware_c_series_2_1_4_178/sensorlib/hw_mpu6050.h: 
+C:/ti/tivaware_c_series_2_1_4_178/sensorlib/mpu6050.h: 
+C:/ti/tivaware_c_series_2_1_4_178/inc/hw_ints.h: 
+C:/ti/tivaware_c_series_2_1_4_178/inc/hw_sysctl.h: 
+C:/ti/tivaware_c_series_2_1_4_178/inc/hw_types.h: 
+C:/ti/tivaware_c_series_2_1_4_178/inc/hw_i2c.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/rom_map.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/debug.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/sysctl.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/string.h: 
+C:/ti/tivaware_c_series_2_1_4_178/utils/uartstdio.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdarg.h: 
+C:/ti/tivaware_c_series_2_1_4_178/IQmath/IQmathLib.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/limits.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdlib.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/math.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/_defs.h: 
---
+++ Midterm/Midterm/Debug/subdir_rules.mk
@@ -0,0 +1,29 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Each subdirectory must supply rules for building sources it contributes
+main.obj: ../main.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: ARM Compiler'
+	"C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="C:/Users/oit/workspace_v7/Midterm" --include_path="C:/ti/tivaware_c_series_2_1_4_178" --include_path="C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include" --define=ccs="ccs" --define=TARGET_IS_BLIZZARD_RB1 --define=PART_TM4C123GH6PM -g --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="main.d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+tm4c123gh6pm_startup_ccs.obj: ../tm4c123gh6pm_startup_ccs.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: ARM Compiler'
+	"C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="C:/Users/oit/workspace_v7/Midterm" --include_path="C:/ti/tivaware_c_series_2_1_4_178" --include_path="C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include" --define=ccs="ccs" --define=TARGET_IS_BLIZZARD_RB1 --define=PART_TM4C123GH6PM -g --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="tm4c123gh6pm_startup_ccs.d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+uartstdio.obj: ../uartstdio.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: ARM Compiler'
+	"C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/bin/armcl" -mv7M4 --code_state=16 --float_support=FPv4SPD16 -me --include_path="C:/Users/oit/workspace_v7/Midterm" --include_path="C:/ti/tivaware_c_series_2_1_4_178" --include_path="C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include" --define=ccs="ccs" --define=TARGET_IS_BLIZZARD_RB1 --define=PART_TM4C123GH6PM -g --gcc --diag_warning=225 --diag_wrap=off --display_error_number --abi=eabi --preproc_with_compile --preproc_dependency="uartstdio.d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+
