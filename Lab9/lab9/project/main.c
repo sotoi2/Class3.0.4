536d484ba6cf26ef5cd1dda208b6e52fb700bdc9
sotoi2 <47646947+sotoi2@users.noreply.github.com>
2019-10-30 20:49:25 -0700

    Update main.c

--- Lab9/lab9/project/main.c
+++ Lab9/lab9/project/main.c
@@ -10,36 +10,38 @@
 #define TARGET_IS_BLIZZARD_RB1
 // used for assigning radians value
 #ifndef M_PI
+
+
 #define M_PI                    3.14159265358979323846
 #endif
 
-#define SERIES_LENGTH 100
+#define SERIES_LENGTH 1000
 
-float gSeriesData[SERIES_LENGTH];
+float gSeriesData[SERIES_LENGTH];// create array of amount wanted of calculations
 
-int32_t i32DataCount = 0;
+int32_t i32DataCount = 0; //initialize data count counter 
 
 int main(void)
 {
-    float fRadians;
+    float fRadians; // radians variable
     //enable fpu calculations
-    ROM_FPULazyStackingEnable();
-    ROM_FPUEnable();
+    ROM_FPULazyStackingEnable();// the function to start avoiding an increase in int latency. 
+    ROM_FPUEnable();// enable the fpu for use
     // set clock
     ROM_SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
     // set value for radians
 
-    fRadians = ((2 * M_PI) / SERIES_LENGTH);
+    fRadians = ((2 * M_PI) / SERIES_LENGTH);// value used for graphing waves (2pi)
     // count for 100 times
-    while(i32DataCount < SERIES_LENGTH)
+    while(i32DataCount < SERIES_LENGTH) // loop for series lenggth times.
     {
         // equation to graph / create
-        gSeriesData[i32DataCount] = sinf( fRadians * i32DataCount + 50*i32DataCount) + 0.5*cosf(fRadians * i32DataCount + 200*i32DataCount);
-
-        i32DataCount++;
+        gSeriesData[i32DataCount] = sinf( fRadians * i32DataCount * 50) + 0.5*cosf(fRadians * i32DataCount * 200);
+ // sets equation to graph (sin(2pi)).
+        i32DataCount++; //increment.
     }
 
-    while(1)
+    while(1)//loop forever for calc
     {
     }
 }
